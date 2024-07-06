08674bba733bdd6c0aaa73feea77878b93ccca75
JoseToledo046 <joseroberto.toledo046@comunidadunir.net>
2024-07-06 07:53:14 -0500

    LOG

--- src/main/java/com/Operador/operador/facade/PeliculaFacade.java
+++ src/main/java/com/Operador/operador/facade/PeliculaFacade.java
@@ -2,6 +2,7 @@ package com.Operador.operador.facade;
 
 import com.Operador.operador.Entidad.model.response.Pelicula;
 import com.Operador.operador.Entidad.model.response.PeliculaResponse;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 
+@Log4j2
 @Slf4j
 public class PeliculaFacade {
     @Value("${orderProduct.url}")
@@ -23,6 +25,9 @@ public class PeliculaFacade {
     public Pelicula getProductDetails(Integer id) {
         try {
             String url = String.format(orderProductUrl+"?idFilm=%d", id);
+            log.info("Busqueda pelicula");
+            log.info(id);
+            log.info(url);
             PeliculaResponse productResponse=restTemplate.getForObject(url, PeliculaResponse.class);
             return productResponse.getPeliculas().get(0);
         } catch (HttpClientErrorException e) {
